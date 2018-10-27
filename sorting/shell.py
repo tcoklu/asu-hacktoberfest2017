902d700299368db72299afc9f2cf49ce36fcdb9f
Tuncay Coklu <tcoklu@gmail.com>
2018-10-27 15:08:01 +0300

    Shell sort modified

--- sorting/shell.py
+++ sorting/shell.py
@@ -1,23 +1,18 @@
 import numpy
 
-def shellSort(input_list):
+def shellSort(numbers):
     
-    gap = len(input_list) // 2
+    gap = len(numbers) // 2
     while gap > 0:
 
-        for i in range(gap, len(input_list)):
-            temp = input_list[i]
+        for i in range(gap, len(numbers)):
+            temp = numbers[i]
             j = i
 # Sort the sub list for this gap
-            while j >= gap and input_list[j - gap] > temp:
-                input_list[j] = input_list[j - gap]
+            while j >= gap and numbers[j - gap] > temp:
+                numbers[j] = numbers[j - gap]
                 j = j-gap
-            input_list[j] = temp
+            numbers[j] = temp
 # Reduce the gap for the next element
         gap = gap//2
-
-# 20 random numbers generated
-nums = numpy.random.randint(100, size=20).tolist()
-# 20 random numbers sorted
-shellSort(nums)
-print(nums)
+    return numbers
